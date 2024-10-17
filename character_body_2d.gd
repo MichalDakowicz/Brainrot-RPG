ba51e5bd9ea93786068dc23b040f3dfc842c8ca4
Krzysztofr1 <krzysztof.rusel@infotech.edu.pl>
2024-10-17 15:21:30 +0200

    movement

---
+++ character_body_2d.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var SPEED = 300.0
+var VELOCITY = Vector2()
+
+func _ready(): 
+	pass
+
+func _physics_process(delta):
+	velocity=Vector2()
+	
+	if Input.is_action_pressed("down"): 
+		velocity.y += SPEED
+	elif Input.is_action_pressed("up"): 
+		velocity.y -= SPEED
+	elif Input.is_action_pressed("left"): 
+		velocity.x -= SPEED
+	elif Input.is_action_pressed("right"): 
+		velocity.x += SPEED	
+		
+	move_and_slide()
