ad98fd44e12a3a81cb74bab54f1afd077c6349a2
Oskar T <raksopod@gmail.com>
2024-12-02 16:04:49 +0100

    chod

--- BrainrotRPG/character_body_2d.gd
+++ BrainrotRPG/character_body_2d.gd
@@ -1,21 +1,42 @@
 extends CharacterBody2D
 
 var SPEED = 100.0
-var VELOCITY = Vector2()
+var last_direction = "down"  # Domyślny kierunek bezczynności
 
-func _ready(): 
-	pass
+@onready var sprite = $AnimatedSprite2D
+
+func _ready():
+	sprite.play("idle_down")  # Domyślna animacja bezczynności
 
 func _physics_process(delta):
-	velocity=Vector2()
+	velocity = Vector2()
+	var is_moving = false
 	
-	if Input.is_action_pressed("down"): 
-		velocity.y += SPEED
-	elif Input.is_action_pressed("up"): 
+	# Wykrywanie kierunku ruchu
+	if Input.is_action_pressed("up"):
 		velocity.y -= SPEED
-	elif Input.is_action_pressed("left"): 
+		sprite.play("back_walk")
+		last_direction = "back"
+		is_moving = true
+	elif Input.is_action_pressed("down"):
+		velocity.y += SPEED
+		sprite.play("front_walk")
+		last_direction = "front"
+		is_moving = true
+	elif Input.is_action_pressed("left"):
 		velocity.x -= SPEED
-	elif Input.is_action_pressed("right"): 
-		velocity.x += SPEED	
-		
+		sprite.play("left_walk")
+		last_direction = "left"
+		is_moving = true
+	elif Input.is_action_pressed("right"):
+		velocity.x += SPEED
+		sprite.play("right_walk")
+		last_direction = "right"
+		is_moving = true
+	
+	# Animacja bezczynności, gdy brak ruchu
+	if not is_moving:
+		sprite.play(last_direction)
+
+	# Poruszanie postaci
 	move_and_slide()
