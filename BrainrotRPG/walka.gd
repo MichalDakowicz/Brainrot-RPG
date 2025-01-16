58c41c0f08efdc1e17888bd1b3fb775b983fcb0e
Oskar T <raksopod@gmail.com>
2025-01-16 09:35:08 +0100

    System Walki - Działa

--- BrainrotRPG/walka.gd
+++ BrainrotRPG/walka.gd
@@ -10,11 +10,10 @@ func _ready():
 	await get_tree().create_timer(0.1).timeout
 	can_change_scene = true
 
-func _on_body_entered(body):
-	# Sprawdzenie, czy obiekt to postać gracza i czy scena może zostać zmieniona
-	if body is CharacterBody2D and can_change_scene:
-		print("Przenoszenie na arenę walki...")
-		call_deferred("change_scene")  # Odroczenie zmiany sceny
-
 func change_scene():
 	get_tree().change_scene_to_file(target_scene)  # Zmiana sceny na arenę walki
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and can_change_scene:
+		print("Przenoszenie na arenę walki...")
+		call_deferred("change_scene")# Replace with function body.
